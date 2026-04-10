07f491dcd8d6b2ef3297ab99d90101f6bb03a393
rand254 <60304966@udst.edu.qa>
2026-04-10 23:32:38 +0300

    Update score.py

--- deployment/score.py
+++ deployment/score.py
@@ -1,1 +1,19 @@
+import json
+import joblib
+import numpy as np
+import os
 
+def init():
+    global model
+    # This path is where Azure puts your model files
+    model_path = os.path.join(os.getenv("AZUREML_MODEL_DIR"), "model/model.pkl")
+    model = joblib.load(model_path)
+
+def run(raw_data):
+    try:
+        data = json.loads(raw_data)["data"]
+        data = np.array(data)
+        result = model.predict(data)
+        return result.tolist()
+    except Exception as e:
+        return str(e)
